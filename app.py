5e47432401c26413560731c9af1bc007af81dcb8
NomiRudevski <nomi.rudevski@gmail.com>
2024-05-27 12:45:25 +0300

    m

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from enum import Enum
 
+class Menu(Enum):
+    ADD = 1
+    PRINT = 2
+    EXIT = 3
 
 class Animal():
     def __init__(self, name, legs, type):
@@ -7,13 +11,12 @@ class Animal():
         self.legs = legs
         self.type = type
 
-    def __str__(self):
+    def __str__(self) -> str:
         return {f"{self.name}, {self.legs}, {self.type}"}
-
-
 
 animal1 = Animal('bob', 4, 'cat')
 animal2 = Animal('petunia', 2, 'ostridge')
 animal3 = Animal('robert', 8, 'spider')
 
-print(animal1)
+if __name__ == "__main___":
+    pass
